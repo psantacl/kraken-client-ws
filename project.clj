8b8f1c9c4af1430f3db0835fd4e3aa54d6dabfb5
Paul Santa Clara <psantaclara@relaynetwork.com>
2011-08-17 13:17:00 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.wol/java-websockets "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]]
+  :java-source-path "src/java"
+)
---
+++ test/java_websockets/test/core.clj
@@ -0,0 +1,6 @@
+(ns java-websockets.test.core
+  (:use [java-websockets.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
